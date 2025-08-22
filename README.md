0cd107a3d28b1dfe63e26c032d1b150b5446d37f
AnderBEz <andersson.beaz4313@gmail.com>
2025-08-21 20:23:25 -0600

    v2, I added the status http control

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 The endpoint is => http://localhost:3000/users
 
 These endpoint return a JSON
+
+START COMAND => npm run dev
+
+ready v2: in this version, I added the HTTP status control, (the status 404 is never read because i added a default value).
--- index.js
+++ index.js
@@ -3,15 +3,24 @@ import express from 'express';
 const app = express();
 const PORT = 3000;
 
-let users = [];
+let users = [
+    { id: 1, name: 'Andersson', lastName: 'Beaz', age: 21, email: 'andersson.beaz4313@alumnos.udg.mx' }
+];
 let idCounter = 0;
 
 
 // get method returns a list of users => returns data in json format
 app.get("/users", (req, res) => {
-    res.json([
-        { id: 1, name: 'Andersson', lastName: 'Beaz', age: 21, email: 'andersson.beaz4313@alumnos.udg.mx' }, users
-    ])
+    try {
+        if (users.length === 0) {
+            return res.status(404).json({ message: "No users found" });
+        }
+
+        res.status(200).json(users);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal server error" });
+    }
 })
 
 
